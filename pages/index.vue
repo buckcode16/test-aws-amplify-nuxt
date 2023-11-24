434bf4b36544a697116cf0d9f0346cd43332e142
buckcode16 <buckcode16@gmail.com>
2023-11-24 21:57:48 +0800

    Update pages/index

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div>Main</div>
+  <NuxtLink to="/login">Login</NuxtLink>
+  <NuxtLink to="/register">Register</NuxtLink>
+  <NuxtLink to="/private">Private</NuxtLink>
+  <NuxtLink to="/public">Public</NuxtLink>
 </template>
 
 <script setup lang="ts"></script>
